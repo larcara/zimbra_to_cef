288c1300428567e244e584f527df1439754c2d71
luca arcara <larcara@gmail.com>
2017-04-26 14:21:38 +0200

    add mailbox match

--- mailbox_match.rb
+++ mailbox_match.rb
@@ -2,8 +2,8 @@ module MailboxMatch
 
 
   TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+)/
-  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<eventID>[\S]+)/
-  MAILBOX_ADDING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\smailop - Adding Message:\sid=(?<message_id>[\d]+),\sMessage-ID=(?<eventID>[\S]+),\sparentId=(?<folder_id>[\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<eventID>[\S]+)/
+  MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\smailop - Adding Message:\sid=(?<message_id>[\d]+),\sMessage-ID=(?<eventID>[\S]+),\sparentId=(?<folder_id>[\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
 
 
 
