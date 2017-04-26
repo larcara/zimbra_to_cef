37fa731f1fe7353bdfc64273fff6313f9c533ad0
luca arcara <larcara@gmail.com>
2017-04-26 11:14:16 +0200

    move postfix_regexp to file add skip regexp

--- postfix_match.rb
+++ postfix_match.rb
@@ -1,11 +1,11 @@
 module PostfixMatch
-  TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<deviceAction>[\w\/]+)\[(?<eventId>[\d]+)\]\:/
-  TO= /#{TIME_AND_PID}\s(?<queue_id>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\s(orig_to=(?<destinationUserName>[^,]+),)?\srelay=(?<relay>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
-  TO_ORIG= /#{TIME_AND_PID}\s(?<queue_id>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\srelay=(?<relay>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
-  FROM= /#{TIME_AND_PID}\s(?<queue_id>[\w]{11,14}):\sfrom=(?<sourceAddress>[^,]+),\ssize=(?<size>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<message>(.*))\)/
-  OTHER= /#{TIME_AND_PID}\s(?<queue_id>[\w]{11,14}):\s(?<data>.+)/
+  TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\[(?<pid_id>[\d]+)\]\:/
+  TO= /#{TIME_AND_PID}\s(?<eventId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\s(orig_to=(?<destinationUserName>[^,]+),)?\srelay=(?<relay>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
+  TO_ORIG= /#{TIME_AND_PID}\s(?<eventId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\srelay=(?<destinationHostName>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
+  FROM= /#{TIME_AND_PID}\s(?<eventId>[\w]{11,14}):\sfrom=(?<sourceAddress>[^,]+),\ssize=(?<fileSize>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<message>(.*))\)/
+  OTHER= /#{TIME_AND_PID}\s(?<eventId>[\w]{11,14}):\s(?<data>.+)/
 
-  REMOVED= /#{TIME_AND_PID}\s(?<queue_id>[\w]{11,14}):\sremoved/
+  REMOVED= /#{TIME_AND_PID}\s(?<eventId>[\w]{11,14}):\sremoved/
   CONNECT= /#{TIME_AND_PID}\s:\s connect from (?<data>.+)/
   DISCONNECT= /#{TIME_AND_PID}\s:\s disconnect from (?<data>.+)/
 
