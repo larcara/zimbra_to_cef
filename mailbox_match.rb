fe08b920277a7174094e8449a1e8dbd1a1c45461
luca arcara <larcara@gmail.com>
2017-04-26 13:11:14 +0200

    add mailbox match

--- mailbox_match.rb
+++ mailbox_match.rb
@@ -1,9 +1,9 @@
 module MailboxMatch
 
 
-  TIME_AND_PID= /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+)/
-  LMTP_DELIVERING = /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<eventID>[\S]+)/
-  MAILBOX_ADDING = /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\smailop - Adding Message:\sid=(?<message_id>[\d]+),\sMessage-ID=(?<eventID>[\S]+),\sparentId=(?<folder_id>[\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+)/
+  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<eventID>[\S]+)/
+  MAILBOX_ADDING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\])\s\[.*\]\smailop - Adding Message:\sid=(?<message_id>[\d]+),\sMessage-ID=(?<eventID>[\S]+),\sparentId=(?<folder_id>[\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
 
 
 
--- postfix_match.rb
+++ postfix_match.rb
@@ -1,5 +1,5 @@
 module PostfixMatch
-  TIME_AND_PID= /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\[(?<pid_id>[\d]+)\]\:/
+  TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\[(?<pid_id>[\d]+)\]\:/
   TO= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\s(orig_to=(?<destinationUserName>[^,]+),)?\srelay=(?<relay>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
   TO_ORIG= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\srelay=(?<destinationHostName>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
   FROM= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sfrom=(?<sourceAddress>[^,]+),\ssize=(?<fileSize>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<message>(.*))\)/
