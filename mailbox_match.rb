ff27c3691203f42cb8d13a19a878e013407b2f25
luca arcara <larcara@gmail.com>
2017-05-03 12:42:29 +0200

    dinamyc add field

--- mailbox_match.rb
+++ mailbox_match.rb
@@ -1,13 +1,15 @@
 module MailboxMatch
 
 
+
+
   TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+)/
-  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<eventId>[\S]+)/
-  MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\smailop - Adding Message:\sid=(?<message_id>[\d]+),\sMessage-ID=(?<eventId>[\S]+),\sparentId=(?<folder_id>[\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  LMTP_DELIVERING = /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<severity>[\w]+)\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<mail_message_id>[\S]+)/
+  MAILBOX_ADDING =  /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<severity>[\w]+)\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\smailop - Adding Message:\sid=(?<zimbra_message_id>[\d]+),\sMessage-ID=(?<mail_message_id>[\S]+),\sparentId=(?<parent_id>[-|\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  SENDIG_TO_MTA = /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<severity>[\w]+)\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\ssmtp - Sending message to MTA at localhost:\sMessage-ID=(?<mail_message_id>[\S]+),\sreplyType=(?<reply_type>[\S]+)/
 
 
-
-  REG_EXPS = [LMTP_DELIVERING,MAILBOX_ADDING]
+  REG_EXPS = [LMTP_DELIVERING,MAILBOX_ADDING,SENDIG_TO_MTA]
   TO_SKIP = []
 
 end
--- postfix_match.rb
+++ postfix_match.rb
@@ -1,12 +1,12 @@
 module PostfixMatch
-  TIME_AND_PID= /(?<startTime>[\w]+\s+[\d]+\s[\d:]+)\s+(?<agentHostName>[\w]+)\s+(?<process_name>[\w\/]+)\[(?<pid_id>[\d]+)\]\:/
-  TO= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\s(orig_to=(?<destinationUserName>[^,]+),)?\srelay=(?<relay>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
-  TO_ORIG= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sto=(?<destinationAddress>[^,]+),\srelay=(?<destinationHostName>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<message>(.*))\)/
-  FROM= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sfrom=(?<sourceAddress>[^,]+),\ssize=(?<fileSize>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<message>(.*))\)/
-  MESSAGE_ID= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\smessage-id=(?<eventId>.+)/
-  OTHER= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\s(?<data>.+)/
+  TIME_AND_PID= /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\[(?<pid>[\d]+)\]\:/
+  TO= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sto=(?<mail_to>[^,]+),\s(orig_to=(?<mail_orig_to>[^,]+),)?\srelay=(?<relay_host>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<data>(.*))\)/
+  TO_ORIG= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sto=(?<mail_to>[^,]+),\srelay=(?<relay_host>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<data>(.*))\)/
+  FROM= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sfrom=(?<mail_from>[^,]+),\ssize=(?<mail_size>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<data>(.*))\)/
+  MESSAGE_ID= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\smessage-id=(?<mail_message_id>.+)/
+  OTHER= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\s(?<data>.+)/
 
-  REMOVED= /#{TIME_AND_PID}\s(?<agentId>[\w]{11,14}):\sremoved/
+  REMOVED= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sremoved/
   CONNECT= /#{TIME_AND_PID}\s:\sconnect from (?<data>.+)/
   DISCONNECT= /#{TIME_AND_PID}\s:\sdisconnect from (?<data>.+)/
 
