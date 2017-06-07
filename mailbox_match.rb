e4d80621e9ef646f9365274acccc8b9488a79cad
luca arcara <larcara@gmail.com>
2017-06-07 10:39:25 +0200

    remove rewind

--- mailbox_match.rb
+++ mailbox_match.rb
@@ -13,11 +13,15 @@ module MailboxMatch
 ##SYSLOG SCHEMA
 
 
+  ###(?<event_time>[\d-]+\s[\d:,]+)\s(?<deviceSeverity>[\w]+)\s+\[(?<process>[\w]+-[\d]+)\]\s\[(?<ip>ip=.*;)?(?<mail_to>name=.*;)?(?<ua>ua=.*;)?\]\s
+
 ##STANDARD SCHEMA
   TIME_AND_PID= /(?<event_time>[\d-]+\s[\d:,]+)\s(?<deviceSeverity>[\w]+)/
-  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<mail_message_id>[\S]+)/
-  MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\smailop - Adding Message:\sid=(?<zimbra_message_id>[\d]+),\sMessage-ID=(?<mail_message_id>[\S]+),\sparentId=(?<parent_id>[-|\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
-  SENDIG_TO_MTA = /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\ssmtp - Sending message to MTA at localhost:\sMessage-ID=(?<mail_message_id>[\S]+),\sreplyType=(?<reply_type>[\S]+)/
+  MAILBOX_NAME_AND_IP = /(?<account_name>name=.*;)?(?<aname>aname=.*;)?(?<ip>ip=.*;)?(?<ua>ua=.*;)?/
+  LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[#{MAILBOX_NAME_AND_IP}\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<mail_message_id>[\S]+)/
+  MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[#{MAILBOX_NAME_AND_IP}\]\smailop - Adding Message:\sid=(?<zimbra_message_id>[\d]+),\sMessage-ID=(?<mail_message_id>[\S]+),\sparentId=(?<parent_id>[-|\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[#{MAILBOX_NAME_AND_IP}\]\smailop - Adding Message:\sid=(?<zimbra_message_id>[\d]+),\sMessage-ID=(?<mail_message_id>[\S]+),\sparentId=(?<parent_id>[-|\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
+  SENDIG_TO_MTA = /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[#{MAILBOX_NAME_AND_IP}\]\ssmtp - Sending message to MTA at localhost:\sMessage-ID=(?<mail_message_id>[\S]+),\sreplyType=(?<reply_type>[\S]+)/
 
 
   REG_EXPS = [LMTP_DELIVERING,MAILBOX_ADDING,SENDIG_TO_MTA]
--- postfix_match.rb
+++ postfix_match.rb
@@ -4,12 +4,13 @@ module PostfixMatch
   TO_ORIG= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sto=(?<mail_to>[^,]+),\srelay=(?<relay_host>[^,]+),\sdelay=(?<delay>[^,]+),\sdelays=(?<delays>[^,]+),\sdsn=(?<dns>[^,]+),\sstatus=(?<status>[\w]+)\s\((?<data>(.*))\)/
   FROM= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sfrom=(?<mail_from>[^,]+),\ssize=(?<mail_size>[^,]+),\snrcpt=(?<nrcpt>[\w]+)\s\((?<data>(.*))\)/
   MESSAGE_ID= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\smessage-id=(?<mail_message_id>.+)/
+  CONNECTION= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sclient=(?<orig_ip>.+)/
   OTHER= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\s(?<data>.+)/
 
   REMOVED= /#{TIME_AND_PID}\s(?<event_id>[\w]{11,14}):\sremoved/
   CONNECT= /#{TIME_AND_PID}\s:\sconnect from (?<data>.+)/
   DISCONNECT= /#{TIME_AND_PID}\s:\sdisconnect from (?<data>.+)/
 
-  REG_EXPS = [MESSAGE_ID,TO,TO_ORIG,FROM,OTHER]
+  REG_EXPS = [MESSAGE_ID,TO,TO_ORIG,FROM,CONNECTION, OTHER]
   TO_SKIP = [REMOVED,CONNECT,DISCONNECT]
 end
