0056dcfb1a763ee7ab4a09c92231c8eebcfa7d8e
luca arcara <larcara@gmail.com>
2017-06-07 14:45:53 +0200

    remove rewind

--- mailbox_match.rb
+++ mailbox_match.rb
@@ -2,7 +2,7 @@ module MailboxMatch
 
 
   module Syslog
-    TIME_AND_PID= /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<severity>[\w]+)/
+    TIME_AND_PID= /(?<event_time>[\w]+\s+[\d]+\s[\d:]+)\s+(?<host_name>[\w]+)\s+(?<process_name>[\w\/]+)\:\s(?<deviceSeverity>[\w]+)/
     LMTP_DELIVERING = /#{TIME_AND_PID}\s+\[(?<process>[\w]+-[\d]+)\]\s\[.*\]\slmtp - Delivering message:\ssize=(?<size>[\d]+)\sbytes, nrcpts=[\d]+,\ssender=(?<sender>[\S]+@[\S]+),\smsgid=(?<mail_message_id>[\S]+)/
     MAILBOX_ADDING =  /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\smailop - Adding Message:\sid=(?<zimbra_message_id>[\d]+),\sMessage-ID=(?<mail_message_id>[\S]+),\sparentId=(?<parent_id>[-|\d]+),\sfolderId=(?<folder_id>[\d]+),\sfolderName=(?<folder_name>[\w]+)./
     SENDIG_TO_MTA = /#{TIME_AND_PID}\s+\[(?<process>[\w|\d]+)-[\d]+(:\/\/.*\/service\/soap\/(?<soap_action>[\S]+))?\]\s\[.*\]\ssmtp - Sending message to MTA at localhost:\sMessage-ID=(?<mail_message_id>[\S]+),\sreplyType=(?<reply_type>[\S]+)/
--- zimbra_to_cef.rb
+++ zimbra_to_cef.rb
@@ -7,6 +7,7 @@ require_relative "./mailbox_match.rb"
 
 @verbose=0
 @file=nil
+SEVERITY = {"NOTICE" => 0, "INFO" => 1, "WARNING" => 2, "ERROR" => 3 }
 cef_event=CEF::Event.new
 opts=GetoptLong.new(
     ["--verbose",       GetoptLong::OPTIONAL_ARGUMENT],
@@ -53,7 +54,13 @@ def match_to_event(match_data, cef_event)
   match_data.names.each do |_field|
     value = match_data[_field]
     field = @maps.has_key?(_field.to_s) ?  @maps[_field] : _field.to_s
-    value = DateTime.parse(value) if field == "event_time"
+    case field
+      when /event_time/i
+        value = DateTime.parse(value)
+      when /severiry/i
+        value = SEVERITY[value.upcase] || 5
+    end
+
     puts "#{field}: #{value}" if @verbose > 1
     method_name =  "#{field}=".to_sym
     if cef_event.respond_to?(method_name)
@@ -66,6 +73,43 @@ def match_to_event(match_data, cef_event)
   end
 end
 
+
+def mailbox_to_cef(line)
+  MailboxMatch::REG_EXPS.each do |reg_exp|
+    puts "testing #{reg_exp}" if @verbose > 2
+    a = line.match(reg_exp)
+    if a
+      cef_event=CEF::Event.new(deviceVendor: @deviceVendor, deviceProduct: @deviceProduct,
+                               deviceEventClassId: "0:event", name: "mailbox event")
+      match_to_event(a, cef_event)
+      #cef_sender.emit(cef_event) if cef_sender
+      puts cef_event.to_s if (cef_sender.nil? || @verbose > 0)
+      return cef_event
+    end
+  end
+  return nil
+end
+
+def postfix_to_cef(line)
+  PostfixMatch::TO_SKIP.each do |reg_exp|
+    puts "testing skipping #{reg_exp}" if @verbose > 2
+    a = line.match(reg_exp)
+    return true  if a
+  end
+  PostfixMatch::REG_EXPS.each do |reg_exp|
+    puts "testing #{reg_exp}" if @verbose > 2
+    a = line.match(reg_exp)
+    if a
+      cef_event=CEF::Event.new(deviceVendor: @deviceVendor, deviceProduct: @deviceProduct, deviceEventClassId: "0:event", name: "postfix event")
+
+      match_to_event(a, cef_event)
+      #cef_sender.emit(cef_event) if cef_sender
+      puts cef_event.to_s if (cef_sender.nil? || @verbose > 0)
+      return cef_event
+    end
+  end
+  return nil
+end
 
 @maps = {}
 @show_unprocessed = false
@@ -130,44 +174,12 @@ end
   @file.interval # 10
   #@file.backward(100)
   @file.tail do |line|
-
-
       cef_event = nil
-      PostfixMatch::TO_SKIP.each do |reg_exp|
-        puts "testing skipping #{reg_exp}" if @verbose > 2
-        a = line.match(reg_exp)
-        if a
-          cef_event = true
-          break
-        end
-      end
-      unless cef_event # skip if skipped!
-        PostfixMatch::REG_EXPS.each do |reg_exp|
-          puts "testing #{reg_exp}" if @verbose > 2
-          a = line.match(reg_exp)
-          if a
-            cef_event=CEF::Event.new(deviceVendor: @deviceVendor, deviceProduct: @deviceProduct, deviceEventClassId: "0:event", name: "postfix event")
 
-            match_to_event(a, cef_event)
-            cef_sender.emit(cef_event) if cef_sender
-            puts cef_event.to_s if (cef_sender.nil? || @verbose > 0)
-            break
-          end
-        end
-        #break if cef_event # skip if found on postfix!
-        MailboxMatch::REG_EXPS.each do |reg_exp|
-          puts "testing #{reg_exp}" if @verbose > 2
-          a = line.match(reg_exp)
-          if a
-            cef_event=CEF::Event.new(deviceVendor: @deviceVendor, deviceProduct: @deviceProduct,
-                                     deviceEventClassId: "0:event", name: "mailbox event")
-            match_to_event(a, cef_event)
-            cef_sender.emit(cef_event) if cef_sender
-            puts cef_event.to_s if (cef_sender.nil? || @verbose > 0)
-            break
-          end
-        end
-      end
+
+      cef_event ||= postfix_to_cef(line)
+      cef_event ||=  mailbox_to_cef(line)
+      cef_sender.emit(cef_event) if cef_sender && cef_event.is_a?(CEF::Event)
       puts line if (cef_event.nil? && @show_unprocessed )
   end
 
